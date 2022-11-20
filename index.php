bddf2e84bb2a2cdda5f75065390ad90dcb20a0d1
Deiby santiago Cristancho <deibysantiagocrist@gmail.com>
2022-11-20 12:00:40 -0500

    cambios

--- index.php
+++ index.php
@@ -38,62 +38,59 @@ $result = mysqli_query($con, $query);
         <input type="number" class="form-control" name="disponible" id="disponible" min="0" max="1" required>
 
         <br><br>
-<tr><td>Imagen</td><td><input type="file" name="imagen" id="imagen"/>
 
         <button type="submit" class="btn btn-sm btn-outline-primary bg-white">Enviar</button>
 
-    </form>
+        <div class="container2">
+            <!-- Tabla -->
 
-    <div class="container2">
-        <!-- Tabla -->
+            <h4>Registro De Libros Escogidos</h4>
+            <table class="">
+                <thead>
+                    <tr class="">
+                        <td>Toma #</td>
+                        <td>Fecha</td>
 
-        <h4>Registro De Libros Escogidos</h4>
-        <table class="">
-            <thead>
-                <tr class="">
-                    <td>Toma #</td>
-                    <td>Fecha</td>
+                        <td>Titulo Libro</td>
 
-                    <td>Titulo Libro</td>
+                        <td>Autor del Libro</td>
 
-                    <td>Autor del Libro</td>
-
-                    <td>Estado</td>
-                    <td colspan="2">Opciones</td>
-                </tr>
-            </thead>
-
-            <tbody class="">
-                <?php
-                if (mysqli_num_rows($result) > 0) {
-                    $pos        = 1;
-                    $datos   = 0;
-
-                    while ($data = mysqli_fetch_assoc($result)) {
-                     //   $muestras = [$data['titulo'], $data['autor'], $data['disponible']];
-                        $fecha_muestra = date_format(date_create($data['fecha_muestra']), 'd-m-Y');
-                ?>
-                        <tr>
-                            <td><?php echo $pos; ?></td>
-                            <td><?php echo $fecha_muestra; ?></td>
-                            <td><?php echo $data['titulo']; ?></td>
-                            <td><?php echo $data['id_autor']; ?></td>
-                            <td><?php echo $data['disponible']; ?></td>
-
-
-                            <td><a href="editar.php?id=<?php echo $data['id']; ?>" class="btn btn-sm btn-outline-warning">Editar</a></td>
-                            <td><a href="eliminar.php?id=<?php echo $data['id']; ?>" class="btn btn-sm btn-outline-danger" value="">Eliminar</a></td>
-                        </tr>
-                    <?php $pos++;
-                    }
-                } else { ?>
-                    <tr>
-                        <td colspan="9">No hay datos</td>
+                        <td>Estado</td>
+                        <td colspan="2">Opciones</td>
                     </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
+                </thead>
+
+                <tbody class="">
+                    <?php
+                    if (mysqli_num_rows($result) > 0) {
+                        $pos        = 1;
+                        $datos   = 0;
+
+                        while ($data = mysqli_fetch_assoc($result)) {
+                            //   $muestras = [$data['titulo'], $data['autor'], $data['disponible']];
+                            $fecha_muestra = date_format(date_create($data['fecha_muestra']), 'd-m-Y');
+                    ?>
+                            <tr>
+                                <td><?php echo $pos; ?></td>
+                                <td><?php echo $fecha_muestra; ?></td>
+                                <td><?php echo $data['titulo']; ?></td>
+                                <td><?php echo $data['id_autor']; ?></td>
+                                <td><?php echo $data['disponible']; ?></td>
+
+
+                                <td><a href="editar.php?id=<?php echo $data['id']; ?>" class="btn btn-sm btn-outline-warning">Editar</a></td>
+                                <td><a href="eliminar.php?id=<?php echo $data['id']; ?>" class="btn btn-sm btn-outline-danger" value="">Eliminar</a></td>
+                            </tr>
+                        <?php $pos++;
+                        }
+                    } else { ?>
+                        <tr>
+                            <td colspan="9">No hay datos</td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
 
 
     </form>
